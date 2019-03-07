377974447b8846df93a98eec218e726850e30e3f
NeelTandel-BTC <neel.tandel@botreetechnologies.com>
2019-03-07 18:49:01 +0530

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :city_id])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: %i[first_name last_name phone_number city_id]
+    )
   end
+
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :cleaner
       new_cleaner_session_path
--- app/controllers/customer_bookings_controller.rb
+++ app/controllers/customer_bookings_controller.rb
@@ -1,6 +1,5 @@
 class CustomerBookingsController < ApplicationController
   before_action :authenticate_customer!
-  
 
   def index
     @bookings = current_customer.bookings
@@ -14,40 +13,35 @@ class CustomerBookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
-  def edit
-
-  end
-
   def create
     @customer_booking = current_customer.bookings.new(booking_params)
-    change_start_end_time()
+    change_start_end_time
 
     city_cleaners = current_customer.city.cleaners
     @city_cleaner_ids = city_cleaners.ids
     if !city_cleaners.empty?
       @bookings = city_cleaners.map { |cleaner| cleaner.bookings }.flatten!
-      find_free_cleaner() unless @bookings.empty? 
+      find_free_cleaner unless @bookings.empty?
       @customer_booking.cleaner_id = @city_cleaner_ids.sample
       if @customer_booking.save
         redirect_to customer_bookings_path
       else
-        flash_message 
-      end  
+        flash_message
+      end
     else
       flash_message
     end
   end
 
   def find_free_cleaner
-    
     free_cleaners = @bookings.select do |booking|
-    @city_cleaner_ids.delete(booking.cleaner_id)  if @customer_booking.start_time.between?(booking.start_time,booking.end_time) ||
-                                                     @customer_booking.end_time.between?(booking.start_time,booking.end_time)       
+    @city_cleaner_ids.delete(booking.cleaner_id) if @customer_booking.start_time.between?(booking.start_time,booking.end_time) ||
+                                                    @customer_booking.end_time.between?(booking.start_time,booking.end_time)       
     end
   end
 
   def flash_message
-    flash[:alert] = @customer_booking.errors.full_messages.join(',') 
+    flash[:alert] = @customer_booking.errors.full_messages.join(',')
     render 'new'
   end
 
@@ -57,8 +51,26 @@ class CustomerBookingsController < ApplicationController
     params.require(:booking).permit(:date, :start_time, :end_time)
   end
 
-  def change_start_end_time()
-    @customer_booking.start_time = @customer_booking.start_time.change(day:@customer_booking.date.day,mont:@customer_booking.date.month,year:@customer_booking.date.year)
-    @customer_booking.end_time = @customer_booking.end_time.change(day:@customer_booking.date.day,mont:@customer_booking.date.month,year:@customer_booking.date.year)
+  def change_start_end_time
+    start_time
+    end_time
+  end
+
+  def end_time
+    @customer_booking.end_time =
+      @customer_booking.end_time.change(
+        day: @customer_booking.date.day,
+        mont: @customer_booking.date.month,
+        year: @customer_booking.date.year
+      )
+  end
+
+  def start_time
+    @customer_booking.start_time =
+      @customer_booking.start_time.change(
+        day: @customer_booking.date.day,
+        mont: @customer_booking.date.month,
+        year: @customer_booking.date.year
+      )
   end
 end
